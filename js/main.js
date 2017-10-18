531807a261baf8a0109c427c6fbe4126e07d08ba
Unknown <yla312@sfu.ca>
2017-10-18 02:37:18 -0700

    jscript: change header theme when scroll down

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@
 $(document).ready(function() {
   $(this).scrollTop(0);
 })
+
+
+// scrollMenu jQuery
+// changing background-color opacity of header
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($ (this).scrollTop() > 0) {
+      $('.darkTheme').css('background-color','rgba(0,0,0,0.9)');
+      $('.lightTheme').css('background-color','rgba(255,255,255,0.9)');
+    } else {
+      $('.darkTheme').css('background-color','rgba(0,0,0,0)');
+      $('.lightTheme').css('background-color','rgba(255,255,255,0.9)');
+    }
+  })
+})
