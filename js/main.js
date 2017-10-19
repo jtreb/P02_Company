b11b35fa53af79b2eceb0617ede14f2cacfa6cb7
Unknown <yla312@sfu.ca>
2017-10-18 22:20:27 -0700

    javascript: adding carousel slideshow & click listener

--- js/main.js
+++ js/main.js
@@ -17,3 +17,31 @@ $(document).ready(function() {
     }
   })
 })
+
+
+//carousel image slideshow
+var slideIndex = 1;
+showImage(slideIndex);
+
+document.getElementById("carouselPrev").addEventListener("click", function() {
+  plusIndex(-1);
+});
+
+document.getElementById("carouselNext").addEventListener("click", function() {
+  plusIndex(+1);
+});
+
+function plusIndex(n) {
+  showImage(slideIndex += n);
+}
+
+function showImage(n) {
+    var i;
+    var x = document.getElementsByClassName("slideImage");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
