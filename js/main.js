e6886f1d0f856940e7f4676667e3109d75e6b309
Unknown <yla312@sfu.ca>
2017-10-27 09:13:24 -0700

    product filter added

--- js/main.js
+++ js/main.js
@@ -66,3 +66,54 @@ function autoShowImage() {
       setTimeout(autoShowImage, 8000); // Change image every 8 seconds
     }
 }
+
+
+
+filterSelection('all');
+
+var inputSearch = document.getElementById("searchInput");
+
+document.getElementById("searchButton").addEventListener("click",
+function () {
+  var inputResult = "";
+  var i;
+  for (i = 0; i < inputSearch.length ;i++) {
+      inputResult += inputSearch.elements[i].value;
+  }
+  inputResult = inputResult.toLowerCase();
+  console.log(inputResult);
+
+  filterSelection(inputResult);
+});
+
+filterSelection('all')
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterProduct");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    removeClassFromElement(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClassToElement(x[i], "show");
+  }
+}
+
+function addClassToElement(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function removeClassFromElement(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
