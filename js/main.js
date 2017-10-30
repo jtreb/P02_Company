62f9d27f78064476e0f27498d8b216fc5ec365dd
Unknown <yla312@sfu.ca>
2017-10-29 19:31:41 -0700

    main.js commenting update

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 $(document).ready(function() {
   $(this).scrollTop(0);
 })
+//---------------------------------------------------------------------------------------
 
 
-// scrollMenu jQuery
+
+
+//--------------------------------------------------------------------- scrollMenu jQuery
 // changing background-color opacity of header
 $(document).ready(function() {
   $(window).scroll(function() {
@@ -17,60 +20,75 @@ $(document).ready(function() {
     }
   })
 })
+//----------------------------------------------------------------- scrollMenu jQuery END
 
 
+
+
+//----------------------------------------------------------- carousel display javaScript
 //carousel image slideshow
 var slideIndex = 1;
 showImage(slideIndex);
 autoShowImage();
 
+// if carousel previous button is present in page, then add click listener
 if (document.getElementById("carouselPrev")) {
   document.getElementById("carouselPrev").addEventListener("click", function() {
     plusIndex(-1);
   });
 }
 
+// if carousel next button is present in page, then add click listener
 if (document.getElementById("carouselNext")) {
   document.getElementById("carouselNext").addEventListener("click", function() {
     plusIndex(+1);
   });
 }
 
+
+// diplay the image by current index
 function plusIndex(n) {
   showImage(slideIndex += n);
 }
 
+// display image updating index
 function showImage(n) {
-    var i;
-    var x = document.getElementsByClassName("slideImage");
-    if (x.length > 0) {
-      if (n > x.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = x.length} ;
-      for (i = 0; i < x.length; i++) {
-          x[i].style.display = "none";
-      }
-      x[slideIndex-1].style.display = "block";
-    }
-}
-
-function autoShowImage() {
-    var i;
-    var x = document.getElementsByClassName("slideImage");
-    if (x.length > 0) {
-      for (i = 0; i < x.length; i++) {
+  var i;
+  var x = document.getElementsByClassName("slideImage");
+  if (x.length > 0) {
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > x.length) {slideIndex = 1}
-      x[slideIndex-1].style.display = "block";
-      setTimeout(autoShowImage, 8000); // Change image every 8 seconds
+    }
+    x[slideIndex-1].style.display = "block";
     }
 }
 
+// auto display image updating index at a period of time
+function autoShowImage() {
+  var i;
+  var x = document.getElementsByClassName("slideImage");
+  if (x.length > 0) {
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(autoShowImage, 8000); // Change image every 8 seconds
+  }
+}
+//------------------------------------------------------- carousel display javaScript END
 
-// product selection filter
+
+
+
+//------------------------------------------------------------- product filter javaScript
+// product selection filter to show all product
 filterSelection('all');
 
+// if search button is present in page, then add click listener
 if (document.getElementById("searchButton")) {
   document.getElementById("searchButton").addEventListener("click",
   function (clickEvent) {
@@ -100,6 +118,7 @@ function filterSelection(c) {
     removeClassFromElement(x[i], "show");
     if (x[i].className.indexOf(c) > -1) addClassToElement(x[i], "show");
   }
+  console.log("input: " + c);
 }
 
 function addClassToElement(element, name) {
@@ -122,3 +141,4 @@ function removeClassFromElement(element, name) {
   }
   element.className = arr1.join(" ");
 }
+//--------------------------------------------------------- product filter javaScript END
